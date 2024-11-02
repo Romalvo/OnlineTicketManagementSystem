07ec5a57b8b81e7e94dd2e0ecaec68de9b012608
Romalvo <roman.voronkov.06@gmail.com>
2024-11-02 15:21:36 +0200

    Updated SupportPersonnel

--- src/main/java/sda/spring/onlineticketmanagementsystem/entity/SupportPersonnel.java
+++ src/main/java/sda/spring/onlineticketmanagementsystem/entity/SupportPersonnel.java
@@ -1,7 +1,25 @@
 package sda.spring.onlineticketmanagementsystem.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
 
 @Entity
-public class SupportPersonnel {
+@Setter
+@Getter
+public class SupportPersonnel extends User {
+
+    @ManyToMany
+    @JoinTable(
+            name="support_personnel_department",
+            joinColumns = @JoinColumn(name = "support_personnel_id"),
+            inverseJoinColumns = @JoinColumn(name = "department_id")
+    )
+
+    private Set<SupportPersonnel> departments;
 }
