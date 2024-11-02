49405a69aebb74d8c4f28a6606be128375517a22
Romalvo <roman.voronkov.06@gmail.com>
2024-11-02 15:19:21 +0200

    Created TokenResponseDto

---
+++ src/main/java/sda/spring/onlineticketmanagementsystem/dto/response/TokenResponseDto.java
@@ -0,0 +1,12 @@
+package sda.spring.onlineticketmanagementsystem.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TokenResponseDto {
+
+    private String token;
+}
