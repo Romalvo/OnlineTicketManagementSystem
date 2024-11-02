351e244a7478daecbb30887000cfb52fa0898a6d
Romalvo <roman.voronkov.06@gmail.com>
2024-11-02 15:20:42 +0200

    Updated Department

--- src/main/java/sda/spring/onlineticketmanagementsystem/entity/Department.java
+++ src/main/java/sda/spring/onlineticketmanagementsystem/entity/Department.java
@@ -1,7 +1,25 @@
 package sda.spring.onlineticketmanagementsystem.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
+@Getter
+@Setter
 public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDateTime createdAt;
+
+    @ManyToMany(mappedBy = "departments")
+    private Set<SupportPersonnel> supportPersonnels;
 }
