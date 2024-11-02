af2fbcf0231024a465596aea8f3926d15e6d5629
Romalvo <roman.voronkov.06@gmail.com>
2024-11-02 15:21:21 +0200

    Updated EmailTestController

--- src/main/java/sda/spring/onlineticketmanagementsystem/controller/EmailTestController.java
+++ src/main/java/sda/spring/onlineticketmanagementsystem/controller/EmailTestController.java
@@ -1,15 +1,25 @@
 package sda.spring.onlineticketmanagementsystem.controller;
 
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import sda.spring.onlineticketmanagementsystem.dto.common.EmailMessageDto;
+import sda.spring.onlineticketmanagementsystem.service.EmailService;
 
 @RestController
 @RequestMapping("/api/v1/email")
 public class EmailTestController {
 
-    @PostMapping
-    public void sendTestEmail(@RequestParam String to, String subject, String body) {}
+    private final EmailService emailService;
+
+    public EmailTestController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    // @RequestBody -> APIs (json)
+    // @ModelAttribute -> html forms (form-data)
+    public void sendTestEmail(@ModelAttribute EmailMessageDto emailMessageDto) {
+        emailService.sendEmail(emailMessageDto);
+    }
 }
