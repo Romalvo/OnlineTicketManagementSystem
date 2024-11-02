8518e8f374f0d6e57d241046856da696a5fdd461
Romalvo <roman.voronkov.06@gmail.com>
2024-11-02 15:20:57 +0200

    Updated EmailService

--- src/main/java/sda/spring/onlineticketmanagementsystem/service/EmailService.java
+++ src/main/java/sda/spring/onlineticketmanagementsystem/service/EmailService.java
@@ -1,7 +1,10 @@
 package sda.spring.onlineticketmanagementsystem.service;
 
 
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import sda.spring.onlineticketmanagementsystem.dto.common.EmailMessageDto;
 
 @Service
 public class EmailService {
@@ -12,7 +15,13 @@ public class EmailService {
         this.mailSender = mailSender;
     }
 
-    public void sendEmail(String to, String subject, String text){
+    public void sendEmail(EmailMessageDto emailMessage){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(emailMessage.getTo());
+        message.setFrom(emailMessage.getFrom());
+        message.setSubject(emailMessage.getSubject());
+        message.setText(emailMessage.getBody());
 
+        mailSender.send(message);
     }
 }
