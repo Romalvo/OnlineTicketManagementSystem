e55e795364247a5e228a0661a6de45898ca9eb7d
Romalvo <roman.voronkov.06@gmail.com>
2024-11-02 15:17:11 +0200

    Created AuthService

---
+++ src/main/java/sda/spring/onlineticketmanagementsystem/service/AuthService.java
@@ -0,0 +1,33 @@
+package sda.spring.onlineticketmanagementsystem.service;
+
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import sda.spring.onlineticketmanagementsystem.repository.UserRepository;
+
+import java.util.Collections;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
+    }
+
+}
