541b4e954f3dedb955510fd53d65d6c54c41956f
Romalvo <roman.voronkov.06@gmail.com>
2024-11-02 15:18:29 +0200

    Created KeyUtil

---
+++ src/main/java/sda/spring/onlineticketmanagementsystem/util/KeyUtil.java
@@ -0,0 +1,23 @@
+package sda.spring.onlineticketmanagementsystem.util;
+
+
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+public class KeyUtil {
+
+    public static PrivateKey getPrivateKeyFromBytes(byte[] keyBytes) throws Exception {
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePrivate(spec);
+    }
+
+    public static PublicKey getPublicKeyFromBytes(byte[] keyBytes) throws Exception {
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePublic(spec);
+    }
+}
