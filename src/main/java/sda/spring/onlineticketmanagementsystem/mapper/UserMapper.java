44b76eadf53c087bc1719710234457f394bfe334
Romalvo <roman.voronkov.06@gmail.com>
2024-11-06 14:22:05 +0200

    Updated User Mapper

--- src/main/java/sda/spring/onlineticketmanagementsystem/mapper/UserMapper.java
+++ src/main/java/sda/spring/onlineticketmanagementsystem/mapper/UserMapper.java
@@ -2,7 +2,10 @@ package sda.spring.onlineticketmanagementsystem.mapper;
 
 import org.springframework.stereotype.Component;
 import sda.spring.onlineticketmanagementsystem.dto.common.UserProfileDto;
-import sda.spring.onlineticketmanagementsystem.dto.request.RegisterRequestDto;
+import sda.spring.onlineticketmanagementsystem.dto.request.RegisterCustomerRequestDto;
+import sda.spring.onlineticketmanagementsystem.dto.request.RegisterSupportPersonnelDto;
+import sda.spring.onlineticketmanagementsystem.entity.Customer;
+import sda.spring.onlineticketmanagementsystem.entity.SupportPersonnel;
 import sda.spring.onlineticketmanagementsystem.entity.User;
 
 import java.time.LocalDateTime;
@@ -10,7 +13,14 @@ import java.time.LocalDateTime;
 @Component
 public class UserMapper {
 
-    public UserProfileDto toUserProfile(User user) {
+    public UserProfileDto toCustomerProfile(User user) {
+        return new UserProfileDto()
+                .setId(user.getId())
+                .setFirstname(user.getFirstname())
+                .setLastname(user.getLastname())
+                .setEmail(user.getEmail());
+    }
+    public UserProfileDto toSupportPersonnelProfile(SupportPersonnel user) {
         return new UserProfileDto()
                 .setId(user.getId())
                 .setFirstname(user.getFirstname())
@@ -18,15 +28,26 @@ public class UserMapper {
                 .setEmail(user.getEmail());
     }
 
-    public User fromRegisterToUserEntity(RegisterRequestDto registerUserRequestDto) {
-        User user = new User();
-        user.setEmail(registerUserRequestDto.getEmail());
-        user.setFirstname(registerUserRequestDto.getFirstname());
-        user.setLastname(registerUserRequestDto.getLastname());
-        user.setPassword(registerUserRequestDto.getPassword());
-        user.setCreatedAt(LocalDateTime.now());
+    public Customer fromRegisterToCustomerEntity(RegisterCustomerRequestDto registerUserRequestDto) {
+        Customer customer = new Customer();
+        customer.setEmail(registerUserRequestDto.getEmail());
+        customer.setFirstname(registerUserRequestDto.getFirstname());
+        customer.setLastname(registerUserRequestDto.getLastname());
+        customer.setPassword(registerUserRequestDto.getPassword());
+        customer.setCreatedAt(LocalDateTime.now());
 
-        return user;
+        return customer;
+    }
+
+    public SupportPersonnel fromRegisterToSupportPersonnelEntity(RegisterSupportPersonnelDto registerSupportPersonnelDto) {
+        SupportPersonnel supportPersonnel = new SupportPersonnel();
+        supportPersonnel.setEmail(registerSupportPersonnelDto.getEmail());
+        supportPersonnel.setFirstname(registerSupportPersonnelDto.getFirstname());
+        supportPersonnel.setLastname(registerSupportPersonnelDto.getLastname());
+        supportPersonnel.setPassword(registerSupportPersonnelDto.getPassword());
+        supportPersonnel.setCreatedAt(LocalDateTime.now());
+
+        return supportPersonnel;
     }
 
     public UserProfileDto fromEntityToUserProfile(User owner) {
